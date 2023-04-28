f163d55110e4a49bf57880d11e259a41c4fd883e
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-28 18:53:18 +0530

    Fectorial mathod test Cases

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Solver do
-    
+    solver = Solver.new
+    it "factorial of 3 should be 6" do
+        expect(solver.factorial(3)).to eq(6)
+    end
+    it "factorial of 5 should be 120" do
+        expect(solver.factorial(3)).to eq(6)
+    end
+    it "factorial of 0 should be 1" do
+        expect(solver.factorial(0)).to eq(1)
+    end
+    it "factorial of 1 should be 1" do
+        expect(solver.factorial(1)).to eq(1)
+    end
 end
